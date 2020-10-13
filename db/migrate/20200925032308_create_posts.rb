f09282bb840728a38117134f06f5f291ab0e4745
Mayuu Furuki <aaa08027744484@gmail.com>
2020-10-13 14:25:35 +0900

    AjaxAppの準備

--- db/migrate/20200925032308_create_posts.rb
+++ db/migrate/20200925032308_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.text :content
-      # カラムの型の指定textの場合、投稿文などのデータ
+      t.boolean :checked
       t.timestamps
     end
   end
